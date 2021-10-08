6afb4d10b4655d069d7f9b0dcc5a6648547aa31b
sercheo87 <vAcpe8-gufnup-tentet>
2021-10-08 18:45:56 -0500

    Initial code

---
+++ src/main/java/com/example/microservice/accounts/MicroserviceAccountsCreateApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservice.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceAccountsCreateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceAccountsCreateApplication.class, args);
+    }
+
+}
